ec10000d5ac6e1f8c5cfaf8e8e6507e8569e25e4
Agroall <110466247+Agroall@users.noreply.github.com>
2024-06-22 23:17:46 +0100

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -23,29 +23,29 @@ vehicle_type_options = ['Coupe', 'SUV', 'Small Car', 'Sedan', 'Convertible', 'Bu
 vehicle_map = {'Coupe': 0, 'SUV': 1, 'Small Car': 2, 'Sedan': 3, 'Convertible': 4, 'Bus': 5, 'Station Wagon': 6, 'Other': 7}
 vehicle_type = st.selectbox('#### What kind of car is it?', vehicle_type_options)
 
-car_year_reg = st.slider('How old in years is the car?', 0, 80)
+car_year_reg = st.slider('#### How old in years is the car?', 0, 80)
 
 gearbox_options = ['Manual', 'Automatic']
 gearbox_map = {'Manual': 0, 'Automatic': 1}
-gearbox = st.selectbox('Automatic or Manual', gearbox_options)
+gearbox = st.selectbox('#### Automatic or Manual', gearbox_options)
 
-horsepower = st.slider('Car Horsepower:', 0, 1600)
+horsepower = st.slider('#### Car Horsepower:', 0, 1600)
 
-kilometer = st.slider('Car Mileage:', 0, 150000)
+kilometer = st.slider('#### Car Mileage:', 0, 150000)
 
 fueltype_options = ['Diesel', 'Petrol', 'Other', 'LPG', 'Hybrid', 'CNG', 'Electric', 'Other']
 fueltype_map = {'Diesel': 0, 'Petrol': 1, 'Other': 2, 'LPG': 3, 'Hybrid': 4, 'CNG': 5, 'Electric': 6, 'Other': 7}
-fueltype = st.selectbox('What kind of Fuel does the car use?', fueltype_options)
+fueltype = st.selectbox('#### What kind of Fuel does the car use?', fueltype_options)
 
 brand_options = ['Budget', 'Economy', 'Luxury']
 brand_map = {'Budget': 2, 'Economy': 0, 'Luxury': 1}  
-brand = st.selectbox('Car price grade?', brand_options)
+brand = st.selectbox('#### Car price grade?', brand_options)
 
 damages_options = ['Yes', 'No']
 damaged_map = {'Yes': 0, 'No': 1}
-damages = st.selectbox('Any unrepaired damage?', damages_options)
+damages = st.selectbox('#### Any unrepaired damage?', damages_options)
 
-adlength = st.slider('How long in days has this car been up for sale?', 0.0000, 400.000, step=0.1)
+adlength = st.slider('#### How long in days has this car been up for sale?', 0.0000, 400.000, step=0.1)
 
 # Convert user choice to German using the maps
 vehicle_type = vehicle_map[vehicle_type]
